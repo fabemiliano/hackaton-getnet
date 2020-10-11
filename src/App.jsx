0f0aa31a3fc37da071d266639181a20da509a168
fabemiliano <fab.emiliano@gmail.com>
2020-10-11 10:12:07 -0300

    purchases

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,16 @@ import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 // import ApiTester from './components/ApiTester';
 import UserRegister from './components/UserRegister';
+import Payment from './components/Payment';
+import Feedback from './components/Feedback'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/register" component={UserRegister} />
+        <Route exact path="/payment" component={Payment} />
+        <Route exact path="/feedback" component={Feedback} />
       </Switch>
     </BrowserRouter>
   );
