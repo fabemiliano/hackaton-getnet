ee531bc184c436b6a7abc5f7f0d9acf1e8e5e963
Cleyton Oliveira <cleytonoliveirapro@gmail.com>
2020-10-11 14:39:38 -0300

    feat: add render goal by settings

--- src/components/FeedbackPage.jsx
+++ src/components/FeedbackPage.jsx
@@ -2,8 +2,55 @@ import React, { Component } from 'react';
 import Logo from './Logo.jsx';
 
 class FeedbackPage extends Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      lastPurchase: '',
+      sumOfPurchase: '',
+    }
+  }
+
+  componentDidMount() {
+    const storage = JSON.parse(localStorage.getItem('customerPurchases'));
+    const purchase = storage.pop();
+    const { purchases, name } = purchase;
+    this.setState({
+      sumOfPurchase: purchases.reduce((acc, value) => acc + value.purchaseValue, 0),
+      lastPurchase: purchases.pop().purchaseValue,
+      name,
+    })
+  }
+
+  renderGoal() {
+    // const promoSettings = JSON.parse(localStorage.getItem('promoSettings'));
+    const promoSettings = {
+      byPoints: true,
+      byValue: false,
+      pointsGoal: 200,
+      valueGoal: 100,
+    }
+    const { byPoints, byValue, pointsGoal, valueGoal } = promoSettings;
+    const { sumOfPurchase } = this.state;
+    if (byPoints) return (
+      <p>
+        Faltam apenas <b>{pointsGoal - sumOfPurchase} pontos</b> para você resgatar seu prêmio.
+      </p>
+    )
+    if (byValue) return (
+        <p>
+          Faltam apenas <b>R$ {valueGoal - sumOfPurchase}</b> para você resgatar seu prêmio.
+        </p>
+    )
+    return (
+      <p>
+        <b>OBRIGADO!</b>
+      </p>
+    )
+  }
+
   render() {
-    const { name = 'Username', score = 50, goal =  100, gift = 'prêmio' } = this.props;
+    const { name, sumOfPurchase } = this.state;
     return (
       <div>
         <p>
@@ -13,11 +60,11 @@ class FeedbackPage extends Component {
           Muito obrigado pela preferência!
         </p>
         <p>
-          Você tem {score} pontos ou reais acumulados!
+          Você tem {sumOfPurchase} acumulados.
         </p>
-        <p>
-          Faltam apenas {goal} para você resgatar seu {gift}.
-        </p>
+        <span>
+          {this.renderGoal()}
+        </span>
         <Logo />
       </div>
     )
