6cf598e2308158f6303a0c460df18a17721cc9f0
fabemiliano <fab.emiliano@gmail.com>
2020-10-11 10:21:41 -0300

    seeder

---
+++ src/services/seeder.js
@@ -0,0 +1,32 @@
+const storage = [
+  {
+    name: 'Jose',
+    cpf: '12345678901',
+    whatsapp: '49989878789',
+    isWhatsapp: true,
+    purchases: [{
+      purchaseValue: 20,
+      purchaseDate: '2020-10-10T13:16:27.891Z',
+    },
+    {
+      purchaseValue: 30,
+      purchaseDate: '2020-10-11T13:16:27.891Z',
+    }],
+  },
+  {
+    name: 'Maria',
+    cpf: '23456789012',
+    whatsapp: '49983478789',
+    isWhatsapp: true,
+    purchases: [{
+      purchaseValue: 40,
+      purchaseDate: '2020-10-10T13:16:27.891Z',
+    },
+    {
+      purchaseValue: 20,
+      purchaseDate: '2020-10-11T13:16:27.891Z',
+    }],
+  },
+];
+
+export default storage;
