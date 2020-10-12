0610f01a0351f77f835c2b0152dfc817fe5bbdd2
fabemiliano <fab.emiliano@gmail.com>
2020-10-12 17:32:07 -0300

    added readme

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
   return (
     <main className="App">
       <section className="mobile-simulated-page">
-        <BrowserRouter>
+        <BrowserRouter basename="/hackaton-getnet">
           <Switch>
             <Route exact path="/dashboard" component={DashBoard} />
             <Route exact path="/register" component={UserRegister} />
