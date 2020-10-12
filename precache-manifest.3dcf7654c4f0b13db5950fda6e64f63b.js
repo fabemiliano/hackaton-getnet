1b6423bd82d12d450cbe69cda3abfdb689620194
fabemiliano <fab.emiliano@gmail.com>
2020-10-12 17:26:22 -0300

    Updates

---
+++ precache-manifest.3dcf7654c4f0b13db5950fda6e64f63b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee87dc8c0b7b091c201ac177dfd1ae68",
+    "url": "/hackaton-getnet/index.html"
+  },
+  {
+    "revision": "2abedbcb42b000312343",
+    "url": "/hackaton-getnet/static/css/main.b8eb8843.chunk.css"
+  },
+  {
+    "revision": "71daa34c9d06984de00d",
+    "url": "/hackaton-getnet/static/js/2.459a4f09.chunk.js"
+  },
+  {
+    "revision": "736f3ad4ee5990319c2a87119f7ebea6",
+    "url": "/hackaton-getnet/static/js/2.459a4f09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2abedbcb42b000312343",
+    "url": "/hackaton-getnet/static/js/main.d4611641.chunk.js"
+  },
+  {
+    "revision": "ddd0b77228db73ca75d4",
+    "url": "/hackaton-getnet/static/js/runtime-main.1c9cb7ef.js"
+  },
+  {
+    "revision": "845bd98d4fc48435596db7a7e80f3759",
+    "url": "/hackaton-getnet/static/media/GetLoyal_r-w.845bd98d.png"
+  },
+  {
+    "revision": "3c95ee7cebca9d7b4f1153781cd5a2be",
+    "url": "/hackaton-getnet/static/media/bg-white.3c95ee7c.png"
+  },
+  {
+    "revision": "47c224ebdb0952d88af0882a9da1eda7",
+    "url": "/hackaton-getnet/static/media/card.47c224eb.png"
+  },
+  {
+    "revision": "d570652780b703fdb645e836d13152a8",
+    "url": "/hackaton-getnet/static/media/footer-dashboard.d5706527.webp"
+  },
+  {
+    "revision": "1fbe9cf235f58f790f83a70dd76afee4",
+    "url": "/hackaton-getnet/static/media/footer-payment.1fbe9cf2.png"
+  },
+  {
+    "revision": "ee791b3cc1b408597f963b31a1687ccb",
+    "url": "/hackaton-getnet/static/media/footer-settings.ee791b3c.png"
+  },
+  {
+    "revision": "e17bb9e4dd281858a4f9b3313ea1ca81",
+    "url": "/hackaton-getnet/static/media/voucher_icon.e17bb9e4.png"
+  }
+]);
