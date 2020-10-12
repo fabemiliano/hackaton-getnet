4107a419ae677afce2e2dea5b652076e28b8c32d
fabemiliano <fab.emiliano@gmail.com>
2020-10-12 15:51:14 -0300

    minor changes

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -2,26 +2,24 @@ import React, { Component } from 'react';
 import BarChart from './BarChart';
 
 export class DashBoard extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
-      data: []
-    }
+      data: [],
+    };
   }
 
   componentDidMount() {
     const data = JSON.parse(localStorage.getItem('customerPurchases'));
-    this.setState({ data })
+    this.setState({ data });
   }
 
   calculateSalesTotal(data) {
-    const purchases = data.map(e => e.purchases)
+    const purchases = data.map((e) => e.purchases);
     const total = purchases.reduce((acc, e) => {
-      const subtotal = e.reduce((acc2, el) => acc2 + Number(el.purchaseValue), 0)
-      return acc + subtotal
-    }, 0)
-    console.log(total)
+      const subtotal = e.reduce((acc2, el) => acc2 + Number(el.purchaseValue), 0);
+      return acc + subtotal;
+    }, 0);
     return total;
   }
 
@@ -29,31 +27,39 @@ export class DashBoard extends Component {
     let topSale;
     data.forEach((e, i) => {
       if (i === 0) {
-        topSale = e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0)
+        topSale = e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0);
       } else {
-        const total = e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0)
-        if (total > topSale) { topSale = total }
+        const total = e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0);
+        if (total > topSale) { topSale = total; }
       }
-    })
-    const bestBuyer = data.filter(e => e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0) === topSale);
+    });
+    const bestBuyer = data.filter((e) => e.purchases.reduce((acc, el) => acc + el.purchaseValue, 0) === topSale);
     return bestBuyer[0];
   }
-
-  
 
   render() {
     const { data } = this.state;
     return (
       <div>
         <h1>Resumo das Vendas</h1>
-        <p>Total de Vendas: R${(this.calculateSalesTotal(data)).toFixed(2)}</p>
-        <p>Total de Clientes Cadastrados: {data.length}</p>
-        {(data.length > 0) && <p>Cliente com maior consumo: {this.calculateBestCustomer(data).name}</p>}
+        <p>
+          Total de Vendas: R$
+          {(this.calculateSalesTotal(data)).toFixed(2)}
+        </p>
+        <p>
+          Total de Clientes Cadastrados:
+          {' '}
+          {data.length}
+        </p>
+        {(data.length > 0) && (
+          <p>Cliente com maior consumo:{this.calculateBestCustomer(data).name}
+          </p>
+        )}
         <p>Gráfico de Venda Diário</p>
         <BarChart />
       </div>
-    )
+    );
   }
 }
 
-export default DashBoard
+export default DashBoard;
