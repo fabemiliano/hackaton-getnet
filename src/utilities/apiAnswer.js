10897d7854ab0d0794afeedc54d4efa9c02a1bf4
fabemiliano <fab.emiliano@gmail.com>
2020-10-12 16:11:21 -0300

    merged with leandros branch

--- src/utilities/apiAnswer.js
+++ src/utilities/apiAnswer.js
@@ -21,7 +21,6 @@ const apiAnswer = [
       "transaction_type": "FULL",
       "number_installments": "1",
       "card": {
-        "number_token": "20d9ab9cbd07d55f4b0e7761aba6d5498f71d00567159ddc9c37ff670df21c49bc33b93309f6c7ecb5064ffb41abe36528cd9410e80b5ce228d3c5596ba54b11",
         "cardholder_name": "Luciano scalfone",
         "expiration_month": "12",
         "expiration_year": "2022"
@@ -51,7 +50,6 @@ const apiAnswer = [
       "transaction_type": "FULL",
       "number_installments": "1",
       "card": {
-        "number_token": "55b40548793b76e449f7102d5b153e43f042c1d54fa917d544b9b3a5927a6b5dd8a5c6fb8baf296b2cfe82a18da016d628e01f073cece0863e7d6644caefda9e",
         "cardholder_name": "Cleyton Oliveira",
         "expiration_month": "12",
         "expiration_year": "2021"
@@ -77,8 +75,6 @@ const apiAnswer = [
     ],
     "debit": {
       "card": {
-        "number_token": "ef979291b6fe5546c5539062ec61016070a4659555fb440c324964d615682cdd2038fc6982639b8138ca8fc9d7af604bdea89115ba99f7e6d02e046d826619ac",
-        "cardholder_name": "Fabiano",
         "expiration_month": "12",
         "expiration_year": "2021"
       }
