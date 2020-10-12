bca93db6afc4f6a00ab45399cc29712435d890d6
LeandroParisi <lepcbelisario@gmail.com>
2020-10-12 11:35:56 -0300

    Customer-management notifier CSS done

--- src/components/Notifier.js
+++ src/components/Notifier.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import HeaderImage from './HeaderImage';
+import './style_sheets/Notifier.css'
 
 class Notifier extends React.Component {
   constructor() {
@@ -13,7 +15,7 @@ class Notifier extends React.Component {
   }
 
   render() {
-    const customers = JSON.parse(localStorage.base);
+    const customers = JSON.parse(localStorage.customerPurchases);
     const cpfReceived = this.props.match.params.cpf;
     const customer = customers.find(customer => customer.cpf === cpfReceived);
     const { name, temperature, whatsapp, isWhatsapp } = customer;
@@ -27,18 +29,21 @@ class Notifier extends React.Component {
     };
 
     return(
-      <div>
-      <h1>Gerenciador de notificações</h1>
-      <p>{`${name} está ${status}!`}</p>
-      <p>{`Total de pontos ${name} está ${status}!`}</p>
-      { (customer.whatsapp != '' && customer.isWhatsapp != false)
-        ? <label htmlFor="msg-notifier">Mensagem de notificação: 
-          <textarea id="msg-notifier" name="msgToCustomer" onChange={this.handleState} placeholder="Insira aqui uma mensagem para seu cliente" />
-        </label> : <p>Não é possível notificar este cliente pois não possui contatos cadastrados.</p>
-      }
-      <div>
-      {(customer.whatsapp != '' && customer.isWhatsapp != false) ? <button onClick={() => window.open(`https://api.whatsapp.com/send?phone=55${whatsapp}&text=${this.state.msgToCustomer}`,'_blank')} >Enviar via whatsapp</button> : ''}
-      </div>
+      <div className="notifier-page">
+        <HeaderImage size="150px" />
+        <h1>Gerenciador de notificações</h1>
+        <p>{`${name} está ${status}!`}</p>
+        <p>{`Total de pontos ${name} está ${status}!`}</p>
+        { (customer.whatsapp != '' && customer.isWhatsapp != false)
+          ? 
+          <div className="notification-input-container">  <label htmlFor="msg-notifier">Mensagem de notificação:
+            </label> 
+            <textarea className="form-control" id="msg-notifier" name="msgToCustomer" onChange={this.handleState} placeholder="Insira aqui uma mensagem para seu cliente" /> 
+          </div>: <p>Não é possível notificar este cliente pois não possui contatos cadastrados.</p>
+        }
+        <div>
+          {(customer.whatsapp != '' && customer.isWhatsapp != false) ? <button className="footer-menu-item" onClick={() => window.open(`https://api.whatsapp.com/send?phone=55${whatsapp}&text=${this.state.msgToCustomer}`,'_blank')} >Enviar via whatsapp</button> : ''}
+        </div>
       </div>
     );
   }
